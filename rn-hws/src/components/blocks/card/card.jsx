b0f537363edb03be1201c4eefe1dee894b09049e
Vuqar <69174569+vugito@users.noreply.github.com>
2024-02-03 02:22:25 +0400

    update HW 3

--- rn-hws/src/components/blocks/card/card.jsx
+++ rn-hws/src/components/blocks/card/card.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import {Image, Text, View} from "react-native";
+import {Image, Text, View, Alert} from "react-native";
 import styles from "../card/styles";
 
 const Card = ({index, imgLink, name, price}) => {
 
+    const handleGetInfo = () => Alert.alert("About product", `Name: ${name} Price: ${price}`, [
+        {text: 'Close'}
+    ]);
+
+    const test =()=> {
+        console.log(imgLink)};
+
     return (
         <>
-            <View style={styles.box} key={index}>
+            <View style={styles.box} key={index} onTouchStart={test}>
                 <View style={styles.imageContainer}>
-                    <Image style={styles.boxImage} source={require('../../../../assets/img/Hot-deals.png')}/>
-                    {/*<Image style={styles.boxImage} source={{uri: imgLink}} />*/}
-                    {/*<Image style={styles.boxImage} source={require(imgLink)} />*/}
+                    {/*<Image style={styles.boxImage} source={require('../../../../assets/img/Hot-deals.png')}/>*/}
+                    <Image style={styles.boxImage} source={imgLink} />
                 </View>
                 <View style={styles.titleContainer}>
                     <Text style={styles.boxTitle}>{name}</Text>
